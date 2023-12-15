1f62eaa4ad0493099cc4acbf2395ce5ac5c733e8
ahmedSaf412 <proahmedsafwat412@gmail.com>
2023-12-15 16:09:40 +0200

    added features

--- Algo.cpp
+++ Algo.cpp
@@ -1,62 +1,102 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include<vector>
-#include<cstdlib>
-#include<list>
-#include<iomanip>
-#include<ctime>
-
+#include <list>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-class graph{
-    public:
-    
-    graph(int vertices){
-        m_vertices = vertices;
-        m_neighbours = new list<int>[vertices];
-    }
-    
-    void follow_reg(int src, int dist);
+class Graph {
+public:
+    Graph(int vertices);
+    void addEdge(int src, int dest);
+    void displayGraph();
 
-    int m_vertices;
-    list<int>* m_neighbours;
-
+private:
+    int numVertices;
+    list<int>* adjList; // Pointer to an array containing adjacency lists
 };
 
-void graph::follow_reg(int src, int dist){
-    m_neighbours->push_back(src);
+Graph::Graph(int vertices) {
+    numVertices = vertices;
+    adjList = new list<int>[vertices];
+}
+
+void Graph::addEdge(int src, int dest) {
+    // Check if source and destination vertices are within the valid range
+    if (src >= 0 && src < numVertices && dest >= 0 && dest < numVertices) {
+        // Add an edge from source to destination
+        adjList[src].push_back(dest);
+    } else {
+        cerr << "Invalid vertices: (" << src << ", " << dest << ")" << endl;
+    }
+}
+
+void Graph::displayGraph() {
+    for (int i = 0; i < numVertices; ++i) {
+        cout << "Vertex " << i << " neighbors:";
+
+        for (const auto& neighbor : adjList[i]) {
+            cout << " " << neighbor;
+        }
+
+        cout << endl;
+    }
 }
 
 
 
 void RandomID2(ofstream& outputfile) {
-    int accs = 60000;
-    
+    int numAccounts = 60000;
+
     if (outputfile.is_open()) {
         srand(static_cast<unsigned int>(time(0)));
-        
-        for (int i = 0; i < accs; ++i) {
-            int ID = 1 + rand() % accs;
+
+        for (int i = 0; i < numAccounts; ++i) {
+            int ID = 1 + rand() % numAccounts;
             outputfile << ID << endl;  // Assuming each ID should be on a new line
         }
-        
-        outputfile.close(); 
+
+        outputfile.close();
         cout << "Dummy IDs generated and saved to dummy_ids.txt." << endl;
-    }
-    else {
+    } else {
         cerr << "Unable to open file for writing." << endl;
     }
 }
 
 
-int main(){
-    graph data(1000);
 
+int main() {
+
+    ifstream inputfile("dummy_ids.txt");
     ofstream outputfile("dummy_ids.txt");
+
+
+    // Generate random dummy IDs and save them to a file
     RandomID2(outputfile);
 
-    return 0;
+
+    Graph userGraph(6000);
+
+
+    // Assuming you want to add some edges to represent "follow" relationships
+
+    if (inputfile.is_open()){
+        srand(static_cast<unsigned int>(time(0)));
+        for (int j{0};j<6000;j++) {
+            
+            int randomUser = rand() % 60000;
+            int randomUser2 = rand() % 60000;
+
+            userGraph.addEdge(randomUser, randomUser2); // User
+        }
+        inputfile.close();
+    } else {
+        cerr << "Unable to open dummy_ids.txt for reading." << endl;
+    }
+
+    // Display the graph
+    userGraph.displayGraph();
+
     return 0;
 }
